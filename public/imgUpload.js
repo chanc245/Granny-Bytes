1b7ff2d42b8e94901207113e8b59e6690f5b40df
Paridhi <paridhigarg27@gmail.com>
2024-04-29 20:18:32 -0400

    bug fix

--- public/imgUpload.js
+++ public/imgUpload.js
@@ -6,6 +6,11 @@ Dropzone.options.myform = {
     document.getElementById("upload").style.backgroundImage =
       "url(" + URL.createObjectURL(file) + ")";
     console.log(file);
+    localStorage.setItem("file", file);
+
+    // let imgDish = JSON.parse(localStorage.getItem("file"));
+    // console.log(imgDish.dataURL);
+
     done();
   },
 };
